2a2f99a07af808e40257e62c9fab3a78e5db108b
haakohsk <haakon0803@gmail.com>
2022-09-21 20:48:06 +0200

    update bangbang.sh

--- bangbang.sh
+++ bangbang.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
-# scrot -a 1,30,400,40 ~/Pictures/test2
+# Parse Terminal window coordinates 
+x=$(xdotool getwindowfocus getwindowgeometry --shell | sed -n 2p | sed 's/[A-Z]//g' | sed 's/\=//g')
+y=$(xdotool getwindowfocus getwindowgeometry --shell | sed -n 3p | sed 's/[A-Z]//g' | sed 's/\=//g')
+w=$(xdotool getwindowfocus getwindowgeometry --shell | sed -n 4p | sed 's/[A-Z]//g' | sed 's/\=//g')
+h=$(xdotool getwindowfocus getwindowgeometry --shell | sed -n 5p | sed 's/[A-Z]//g' | sed 's/\=//g')
 
-# Parse Terminal window coordinates
-# Parse coordinates for X
-xdotool getwindowfocus getwindowgeometry --shell | sed -n 2p | sed 's/[A-Z]//g' | sed 's/\=//g'
-# Parse coordinates for Y
-xdotool getwindowfocus getwindowgeometry --shell | sed -n 3p | sed 's/[A-Z]//g' | sed 's/\=//g'
-# Parse coordinates for WIDTH
-xdotool getwindowfocus getwindowgeometry --shell | sed -n 4p | sed 's/[A-Z]//g' | sed 's/\=//g'
-# Parse coordinates for HEIGHT
-xdotool getwindowfocus getwindowgeometry --shell | sed -n 5p | sed 's/[A-Z]//g' | sed 's/\=//g'
+# Take screenshot of terminal window based on x,y,w,h
+scrot -a $x,$y,$w,$h ~/Pictures/bangbang
