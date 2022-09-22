9e5b159d87aee983c7753833f3b2696ec6383a69
haakohsk <haakon0803@gmail.com>
2022-09-22 11:56:54 +0200

    Update bangbang.sh

--- bangbang.sh
+++ bangbang.sh
@@ -6,5 +6,5 @@ y=$(xdotool getwindowfocus getwindowgeometry --shell | sed -n 3p | sed 's/[A-Z]/
 w=$(xdotool getwindowfocus getwindowgeometry --shell | sed -n 4p | sed 's/[A-Z]//g' | sed 's/\=//g')
 h=$(xdotool getwindowfocus getwindowgeometry --shell | sed -n 5p | sed 's/[A-Z]//g' | sed 's/\=//g')
 
-# Take screenshot of terminal window based on x,y,w,h
-scrot -a $x,$y,$w,$h ~/Pictures/bangbang
+# Take screenshot of terminal window based on x,y,w,h - Also includes date and time in file name
+scrot -a $x,$y,$w,$h ~/Pictures/screenshots/'screenshot_%Y%m%d_%H%M%S.png'
